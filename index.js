c37a63776a93c4f7f1a357473c01c81198c3650f
etcadinfinitum <lizzy.presland@gmail.com>
2020-03-30 11:24:06 -0700

    Port asynchronous routines and GH client

--- index.js
+++ index.js
@@ -1,64 +1,45 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const { Octokit } = require('@octokit/rest');
 
-const octokit = new Octokit({
-    auth: core.getInput('INVITATION_TOKEN'),
-    userAgent: 'inviteme v1.0',
-    // omitting TimeZone: https://developer.github.com/v3/#using-the-last-known-timezone-for-the-user
-});
-
-async function createComment(owner, repo, issue_number, body) {
-    const { data: commentResponse } = await octokit.issues.createComment({
-        owner,
-        repo,
-        issue_number,
-        body,
-    });
-}
-
-async function addCollaborator(owner, repo, username, permission) {
-    if (permission) {
-        const { data: addedCollaborator } = await octokit.repos.addCollaborator({
-            owner,
-            repo,
-            username,
-            permission,
-        });
-        return addedCollaborator;
-    } else {
-        const { data: addedCollaborator } = await octokit.repos.addCollaborator({
-            owner,
-            repo,
-            username
-        });
-        return addedCollaborator;
+async function run() {
+    try {
+        // create Octokit client
+        const token = core.getInput('INVITATION_TOKEN');
+        const octokit = new github.GitHub(token);
+        // get comment
+        const thisComment = github.context.payload['comment']['body'];
+        if (comment.startsWith('inviteme!')) {
+            /* begin core invitation logic */
+            // TODO: add support for mentioning a different user
+            // determine variable values
+            let thisRepo = github.context.payload['repository']['name'];    // e.g. 'inviteme'
+            let thisUsername = github.context.payload['comment']['user']['login'];  // e.g. 'etcadinfinitum'
+            let thisOwner = github.context.payload['repository']['owner']['name'];
+            let thisIssueNumber = github.context.payload['issue']['number'];
+            let thisPermission = null;
+            // get response for addCollaborator call
+            const { data: addedCollaborator } = await octokit.repos.addCollaborator({
+                owner: thisOwner,
+                repo: thisRepo,
+                username: thisUsername
+            });
+            // more variable values
+            let thisInviteLink = addedCollaborator['html_url'];
+            let thisBody = '@' + username + ' ' + invite_link;
+            // comment on issue with invitation link for repo
+            const { data: commentResponse } = await octokit.issues.createComment({
+                owner: thisOwner,
+                repo: thisRepo,
+                issue_number: thisIssueNumber,
+                body: thisBody,
+            });
+        } else {
+            console.log('User comment is not an invitation request. Exiting.');
+        }
+    } catch (error) {
+        console.log('ERROR: ' + error.message);
+        core.setFailed(error.message);
     }
 }
 
-try {
-    const token = core.getInput('INVITATION_TOKEN');
-    const comment = github.context.payload['comment']['body'];
-    if (comment.startsWith('inviteme!')) {
-        /* begin core invitation logic */
-        // TODO: add support for mentioning a different user
-        // determine variable values
-        let repo = github.context.payload['repository']['name'];    // e.g. 'inviteme'
-        let username = github.context.payload['comment']['user']['login'];  // e.g. 'etcadinfinitum'
-        let owner = github.context.payload['repository']['owner']['name'];
-        let issue_number = github.context.payload['issue']['number'];
-        let permission = null;
-        // get response for addCollaborator call
-        let addedCollaborator = addCollaborator(owner, repo, username, permission);
-        // more variable values
-        let invite_link = addedCollaborator['html_url'];
-        let body = '@' + username + ' ' + invite_link;
-        // comment on issue with invitation link for repo
-        let commentResponse = createComment(owner, repo, issue_number, body);
-    } else {
-        console.log('User comment is not an invitation request. Exiting.');
-    }
-} catch (error) {
-    console.log('ERROR: ' + error.message);
-    core.setFailed(error.message);
-}
+run()
